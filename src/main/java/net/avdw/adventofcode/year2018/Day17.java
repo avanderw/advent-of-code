4d1dad391896819a94da889b7dcb5fba996f4902
Andrew van der Westhuizen <avanderw@gmail.com>
2018-12-21 15:40:11 +0200

    completed day 17

--- src/main/java/net/avdw/adventofcode/year2018/Day17.java
+++ src/main/java/net/avdw/adventofcode/year2018/Day17.java
@@ -18,6 +18,7 @@ public class Day17 {
         Configurator.defaultConfig().level(Level.TRACE).formatPattern("{level}:\t{message}").activate();
 
         Pattern linePattern = Pattern.compile("([x|y])=(\\d+), ([x|y])=(\\d+)..(\\d+)");
+//        URL inputUrl = Day17.class.getResource("day17-input-test.txt");
         URL inputUrl = Day17.class.getResource("day17-input.txt");
         Scanner scanner = new Scanner(new File(inputUrl.toURI()));
         List<Vector2D> clayPoints = new ArrayList();
@@ -60,10 +61,10 @@ public class Day17 {
                 }
             }
         }
-        Vector2D topLeft = new Vector2D(minX, minY);
-        Vector2D bottomRight = new Vector2D(maxX, maxY);
+        Vector2D topLeft = new Vector2D(minX-1, minY);
+        Vector2D bottomRight = new Vector2D(maxX+1, maxY);
         Vector2D box = bottomRight.subtract(topLeft);
-        clayPoints.sort(Comparator.<Vector2D>comparingDouble(c -> c.getY()).thenComparingDouble(c -> c.getX()));
+        clayPoints.sort(Comparator.comparingDouble(Vector2D::getY).thenComparingDouble(Vector2D::getX));
         Logger.debug(clayPoints);
         Logger.info("identified {} clay points", clayPoints.size());
         Logger.info("grid top left = {}, bottom right = {}, width = {}, height = {}", topLeft, bottomRight, box.getX(), box.getY());
@@ -79,24 +80,100 @@ public class Day17 {
             }
         }
 
-        int count = 0;
-        List<Vector2D> waterPoints = new ArrayList<>();
-        waterPoints.add(new Vector2D(500-minX, 0));
-        while (!waterPoints.isEmpty() && count < 10) {
-            for (int i = 0; i < waterPoints.size(); i++) {
-                Vector2D waterPoint = waterPoints.get(i);
-                int y = (int) waterPoint.getY();
-                int x = (int) waterPoint.getX();
-                map[y][x] = '|';
-                if (map[y + 1][x] == '#' || map[y + 1][x] == '~') {
+        growDown(map, new Vector2D(500 - minX, 0));
 
-                } else {
-                    map[y + 1][x] = '|';
-                    waterPoints.set(i, new Vector2D(x, y + 1));
-                    System.out.println(waterPoint);
+        int count1 = 0, count2=0;
+        for (int y = 0; y < map.length; y++) {
+            for (int x = 0; x < map[y].length; x++) {
+                if (y != 0 && map[y][x] == '|' && map[y-1][x] == '~') {
+                    map[y][x] = '~';
+                }
+                System.out.print(map[y][x]);
+                if (map[y][x] == '|') {
+                    count1++;
+                } else if (map[y][x] == '~') {
+                    count2++;
                 }
             }
-            count++;
+            System.out.println();
+        }
+        System.out.println(String.format("%s + %s = %s", count1, count2, count1 + count2));
+    }
+
+    private static void growDown(Character[][] map, Vector2D p) {
+        int x = (int) p.getX();
+        int y = (int) p.getY();
+        map[y][x] = '|';
+
+        if (y + 1 < map.length) {
+            switch (map[y + 1][x]) {
+                case '.':
+                    growDown(map, new Vector2D(x, y + 1));
+                    break;
+                case '~':
+                case '#':
+                    Vector2D left = null;
+                    boolean hasLeftWall = false;
+                    for (int xx = x - 1; xx >= 0; xx--) {
+                        if (map[y][xx] == '#' && (map[y + 1][xx] == '#' || (map[y + 1][xx] == '~'))) {
+                            hasLeftWall = true;
+                            left = new Vector2D(xx, y);
+                            break;
+                        } else if ((map[y][xx] == '.' || map[y + 1][xx] == '|') && (map[y + 1][xx] == '.' || map[y + 1][xx] == '|')) {
+                            left = new Vector2D(xx, y);
+                            break;
+                        }
+                    }
+                    Vector2D right = null;
+                    boolean hasRightWall = false;
+                    for (int xx = x + 1; xx < map[y].length; xx++) {
+                        if (map[y][xx] == '#' && (map[y + 1][xx] == '#' || (map[y + 1][xx] == '~'))) {
+                            right = new Vector2D(xx, y);
+                            hasRightWall = true;
+                            break;
+                        } else if ((map[y][xx] == '.' || map[y + 1][xx] == '|') && (map[y + 1][xx] == '.' || map[y + 1][xx] == '|')) {
+                            right = new Vector2D(xx, y);
+                            break;
+                        }
+                    }
+
+                    if (hasLeftWall && hasRightWall) {
+                        map[y][x] = '~';
+                        for (int xx = (int) left.getX() + 1; xx < (int) right.getX(); xx++) {
+                            map[y][xx] = '~';
+                        }
+                        growDown(map, new Vector2D(x, y - 1));
+                    }
+
+                    if (!hasLeftWall && hasRightWall) {
+                        for (int xx = (int) left.getX() + 1; xx < (int) right.getX(); xx++) {
+                            map[y][xx] = '|';
+                        }
+                        growDown(map, left);
+                    }
+                    if (hasLeftWall && !hasRightWall) {
+                        for (int xx = (int) left.getX() + 1; xx < (int) right.getX(); xx++) {
+                            map[y][xx] = '|';
+                        }
+                        growDown(map, right);
+                    }
+                    if (!hasLeftWall && !hasRightWall) {
+                        if (left != null) {
+                            growDown(map, left);
+                        } else {
+                            left = new Vector2D(0, y);
+                        }
+                        if (right != null) {
+                            growDown(map, right);
+                        } else {
+                            right = new Vector2D(map[y].length,y);
+                        }
+                        for (int xx = (int) left.getX(); xx < (int) right.getX(); xx++) {
+                                map[y][xx] = '|';
+                            }
+                    }
+                    break;
+            }
         }
     }
 }
