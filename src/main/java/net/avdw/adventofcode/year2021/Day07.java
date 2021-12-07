d331d198524fb46c222f2bb2a591f7b946f34591
Andrew van der Westhuizen <avanderw@gmail.com>
2021-12-07 10:26:05 +0200

    Add 2021-12-07

---
+++ src/main/java/net/avdw/adventofcode/year2021/Day07.java
@@ -0,0 +1,75 @@
+package net.avdw.adventofcode.year2021;
+
+import lombok.SneakyThrows;
+import net.avdw.adventofcode.Day;
+import net.avdw.adventofcode.Runner;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
+import java.util.*;
+
+public class Day07 extends Day {
+    public static void main(String[] args) {
+        Runner.run(new Day07(), 5000);
+    }
+
+    private long fuel(int move) {
+        long sum = 0;
+        for (int cost = 1; cost <= move; cost++) {
+            sum += cost;
+        }
+
+        return sum;
+    }
+
+    @SneakyThrows
+    public String part01() {
+        Scanner scanner = new Scanner(getInput());
+        List<Integer> input = new ArrayList<>(1000);
+        for (String str : scanner.nextLine().split(",")) {
+            input.add(Integer.parseInt(str));
+        }
+
+        int max = input.stream().max(Comparator.comparingInt(i->i)).orElseThrow();
+        int min = input.stream().min(Comparator.comparingInt(i->i)).orElseThrow();
+        Map<Integer, Long> moves = new HashMap<>();
+        for (int target = min; target < max; target++) {
+            if (moves.containsKey(target)) {
+                continue;
+            }
+
+            long sum = 0;
+            for (int i = 0; i < 1000; i++) {
+                sum += Math.abs(input.get(i) - target);
+            }
+            moves.put(target, sum);
+        }
+
+        return "" + moves.values().stream().min(Comparator.comparingLong(l->l)).orElseThrow();
+    }
+
+    @SneakyThrows
+    public String part02() {
+        Scanner scanner = new Scanner(getInput());
+        List<Integer> input = new ArrayList<>(1000);
+        for (String str : scanner.nextLine().split(",")) {
+            input.add(Integer.parseInt(str));
+        }
+
+        int max = input.stream().max(Comparator.comparingInt(i->i)).orElseThrow();
+        int min = input.stream().min(Comparator.comparingInt(i->i)).orElseThrow();
+        Map<Integer, Long> moves = new HashMap<>();
+        for (int target = min; target < max; target++) {
+            if (moves.containsKey(target)) {
+                continue;
+            }
+
+            long sum = 0;
+            for (int i = 0; i < 1000; i++) {
+                sum += fuel(Math.abs(input.get(i) - target));
+            }
+            moves.put(target, sum);
+        }
+
+        return "" + moves.values().stream().min(Comparator.comparingLong(l->l)).orElseThrow();
+    }
+}
